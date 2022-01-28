5f83ed7d858a547ad0404b06127a86528fa95d16
BuildTools <unconfigured@null.spigotmc.org>
2022-01-28 23:02:59 +0500

    HW

--- Car/Source.cpp
+++ Car/Source.cpp
@@ -4,8 +4,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-#define MAX_TANK_VOLUME 40
-#define MIN_TANK_VOLUME 80
+#define MAX_TANK_VOLUME 80
+#define MIN_TANK_VOLUME 40
 
 class Tank
 {
@@ -28,9 +28,9 @@ public:
 
 	double fill(double fuel)
 	{
-		if(fuel_level+fuel>=0 && fuel_level+fuel <=VOLUME)this->fuel_level += fuel;
-		if (fuel_level + fuel > VOLUME)fuel_level = VOLUME;
-		return fuel_level;
+		if (fuel_level + fuel <= 0)return fuel_level = 0;
+		else if (fuel_level + fuel >= VOLUME)return fuel_level = VOLUME;
+		else return fuel_level += fuel;
 	}
 	double give_fuel(double fuel)
 	{
@@ -51,4 +51,11 @@ void main()
 	setlocale(LC_ALL, "rus");
 	Tank tank(120);
 	tank.info();
+	int fuel;
+	while (true)
+	{
+		cout << "ֲגוהטעו מבתול: "; cin >> fuel;
+		tank.fill(fuel);
+		tank.info();
+	}
 }
