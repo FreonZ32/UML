d43a42811508dd836d7d7f45aaf1d84530096b9a
BuildTools <unconfigured@null.spigotmc.org>
2022-02-15 23:46:06 +0500

    HW

---
+++ MyWindow/Source.cpp
@@ -0,0 +1,19 @@
+#include<Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevList, LPSTR lpCmdLine, INT nCmdShow)
+{
+	WNDCLASSEX wc;
+    wc.cbSize = sizeof(WNDCLASS);
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = 
+    if (!RegisterClassEx(&wc)) {
+        MessageBox(NULL,"Не получилось зарегистрировать класс!","Ошибка", MB_OK);
+        return NULL; 
+    }
+}
