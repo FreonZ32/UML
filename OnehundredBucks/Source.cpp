a37b3478a6a8cf8d7c460e0c94cd56ec4afa8ba2
BuildTools <unconfigured@null.spigotmc.org>
2022-03-08 19:01:42 +0500

    HW

--- OnehundredBucks/Source.cpp
+++ OnehundredBucks/Source.cpp
@@ -1,26 +1,28 @@
 #include<iostream>
 #include<math.h>
+#include<fstream>
 using namespace std;
 
 
 void main()
 {
+	srand(time(NULL));
+	ofstream WriteToFile("File.txt");
 	double x, y, z;
 	int i = 0;
 	while (i != 1000)
 	{
-		x = 1 + (rand() % 10) / 10.;
-		for (int i = 0; i < 3; i++, x = (x + (rand() % 10)) * 0.1);
-		y = 1 + (rand() % 10) / 10.;
-		for (int i = 0; i < 3; i++, y = (y + (rand() % 10)) * 0.1);
-		if ((pow(x, 2) + pow(y, 2)) < 1)
+		x = (rand() % 10000) / 10000.;
+		y = (rand() % 10000) / 10000.;
+		if (pow(x, 2) + pow(y, 2) < 1 && x!=0 && y!=0)
 		{
 			z = sqrt(1 - (pow(x, 2) + pow(y, 2)));
 			if ((z * 10000 - int(z * 10000)) == 0)
 			{
-				cout << x << endl;
-			i++;
+				WriteToFile << x << " " << y << " " << z << endl;
+				i++;
 			}
 		}
 	}
+	WriteToFile.close();
 }
